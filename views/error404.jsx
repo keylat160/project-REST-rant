5432e7c4089bb4403abb6be4532384f1079a8a1d
keylat160 <keylat160@gmail.com>
2024-02-19 21:17:12 -0800

    updating index

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,7 @@ function error404 () {
         <Default>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
+                <img src= 'https://sitechecker.pro/wp-content/uploads/2023/06/404-status-code.png' alt= 'error'/>
                 <p>Oops, sorry, we can't find this page!</p>
             </main>
         </Default>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,20 +4,27 @@ const Default = require('../layout/Default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
         return (
-            <div>
-                <h2>{place.name}</h2>
+            <div key={place.name} className='col-sm-6'>
+                <h2>
+                <a href={`/places/${place.id}`}>{place.name}</a>
+                </h2>
+                <p>{place.cuisines}</p>
                 <img src={place.pic} alt={place.name}/>
+                <p className='text-center'>
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
-        )
-    })
+    
+        );
+    });
     return (
         <Default><main>
-            <h1>PLACES INDEX PAGE</h1>
-            {placesFormatted}
+            <h1>Places to Rant or Rave About</h1>
+            <div className='row'>{placesFormatted}</div>
             </main>
             </Default>
-    )
-}
+    );
+};
 module.exports = index;
   
   
